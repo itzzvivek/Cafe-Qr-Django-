58978d2167445bb0804dbdf20c10dca567841742
Vivek Warkade <vivekwarkade000@gmail.com>
2024-05-14 17:07:15 +0530

    fix menulist

--- cafeAdmin/models.py
+++ cafeAdmin/models.py
@@ -13,7 +13,7 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to='menu_images', blank=True)
     max_price = models.DecimalField(max_digits=5, decimal_places=2)
     min_price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='items', default="")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=False, null=True)
 
     def __str__(self):
         return self.name
