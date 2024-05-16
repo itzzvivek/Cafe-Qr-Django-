6fd276e85813bcf92841040e16111a30ffd3e015
Vivek Warkade <vivekwarkade000@gmail.com>
2024-05-16 23:11:10 +0530

    changes

--- cafeAdmin/models.py
+++ cafeAdmin/models.py
@@ -13,10 +13,29 @@ class MenuItem(models.Model):
     image = models.ImageField(upload_to='menu_images', blank=True)
     max_price = models.DecimalField(max_digits=5, decimal_places=2)
     min_price = models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    slug = models.SlugField(default="False")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=False, null=True)
 
     def __str__(self):
-        return self.name
+        return self.nam
+
+    def get_add_to_cart(self):
+        return reverse("core:add-to-cart", kwargs={
+            'slug': self.slug
+        })
+
+    def get_remove_from_cart(self):
+        return reverse("core:remove-from-cart", kwargs={
+            "slug": self.slug
+        })
 
 
+class OrderItem(models.Model):
+    ordered = models.BooleanField(default=False)
+    item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.Integerfield(defualt=1)
 
+    def __str__(self):
+        return f"(self.quantity) of {self.item.title}"
+
+    def 
