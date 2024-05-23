ba93c077e9ba27e3d731cf1a093831c12d0d2ea9
Vivek Warkade <vivekwarkade000@gmail.com>
2024-05-23 15:42:53 +0530

    remove single itme from cart

--- core/views.py
+++ core/views.py
@@ -87,5 +87,33 @@ def remove_from_cart(request, slug):
         return redirect("/", slug=slug)
 
 
+def remove_single_item_from_cart(request, slug):
+    item = get_object_or_404(Item, slug=slug)
+    order_qs = Order.objects.filter(
+        user=request.user,
+        ordered=False
+    )
+    if order_qs.exists():
+        order = order_qs[0]
+        # Check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = OrderItem.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            if order_item.quantity > 1:
+                order_item.quantity -= 1
+                order_item.save()
+            else:
+                order.items.remove(order_item)
+            message.info(request, "This item was removed from your cart.")
+            return redirect("user:order-summary")
+        else:
+            messages.info(request, "This item was not in your cart")
+            return redirect("user:order-summary")
+    else:
+        messages.info(request, "You do not have an active order")
+        return redirect("core:menu", slug=slug)
 
 
