483cda6586242005d1f7e8e04ba6338879f46762
Vivek Warkade <vivekwarkade000@gmail.com>
2024-05-27 21:26:16 +0530

    issue with remove single item

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import menu_view, add_to_cart, OrderSummaryView, remove_from_cart
+from .views import menu_view, OrderSummaryView, add_to_cart, remove_single_item_from_cart, remove_from_cart
 
 app_name = 'core'
 
@@ -7,5 +7,6 @@ urlpatterns = [
     path('menu/', menu_view, name='menu'),
     path('order-summary/', OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-cart/<slug>/', add_to_cart, name='add-to-cart'),
+    path('remove-single-item-from-cart/<slug>/', remove_single_item_from_cart, name='remove-single-item-from-cart'),
     path('remove-from-cart/<slug>/', remove_from_cart, name="remove-from-cart"),
 ]
